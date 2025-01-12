f2eb02cc33247f965d9e4c78ff04a0d7bfdceb0a
awang104 <63938733+awang104-2@users.noreply.github.com>
2025-01-12 12:57:36 -0800

    foo

--- src/simulation.py
+++ src/simulation.py
@@ -4,18 +4,36 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    bodies, N = ([], 10)
-    for _ in range(N):
-        body = Body(mass=np.random.rand()*20, position=np.random.rand(3) * 30, velocity=np.random.rand(3) * 0)
-        bodies.append(body)
+    bodies, N = ([], 3)
+    bodies.append(Body(mass=1, position=np.zeros(3), velocity=np.zeros(3)))
+    bodies.append(Body(mass=1, position=np.array([1, 1, 0]), velocity=np.array([0, 0, 0])))
+    bodies.append(Body(mass=1, position=np.array([-5, -5, 0]), velocity=np.array([0, 0, 0])))
     dynamics = Dynamics(bodies)
-    for _ in range(100):
+    energies = []
+    for _ in range(1000):
         plt.clf()
         kinematics = dynamics(0.1)
         for body in bodies:
             x, y, z = body('x')
             plt.scatter(x, y)
-            plt.xlim(-30, 30)
-            plt.ylim(-30, 30)
-        plt.pause(0.1)
+            plt.xlim(-10, 10)
+            plt.ylim(-10, 10)
+        plt.pause(0.01)
+        body = bodies[0]
+        other_bodies = [x for x in bodies if not x == body]
+        e = body.calculate_total_energies(other_bodies)
+        energies.append(e)
+    PE, KE, E, t = ([], [], [], [])
+    for i, energy in enumerate(energies):
+        t.append(i)
+        KE.append(energy['KE'])
+        PE.append(energy['PE'])
+        E.append(energy['E'])
+    KE = np.array(KE)
+    PE = np.array(PE)
+    plt.clf()
+    plt.plot(t, KE)
+    plt.plot(t, PE)
+    plt.plot(t, E)
+    plt.legend(['KE', 'PE', 'E'])
     plt.show()
