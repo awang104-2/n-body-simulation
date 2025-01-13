8b145536dcd53eb40a6336e7d7a4ad05cef38c0c
awang104 <63938733+awang104-2@users.noreply.github.com>
2025-01-12 19:50:40 -0800

    Reworked dynamics.py
    Abandoned class structure in favor of dictionaries and functions.

--- src/simulation.py
+++ src/simulation.py
@@ -1,39 +1,23 @@
-from dynamics import Body, Dynamics
+from dynamics import *
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+def randomly_define_nbody(N):
+    bodies = []
+    for _ in range(N):
+        mass, position = (np.random.rand(), -10 + np.random.rand(3) * 20)
+        bodies.append(define_body(mass, position))
+    return bodies
+
+
+def main():
+    N = 100
+    nbody = randomly_define_nbody(N)
+    print('original\n', nbody)
+    nbody = leapfrog(nbody, gravity, 5, True)
+    print('new\n', nbody)
+
+
 if __name__ == '__main__':
-    bodies, N = ([], 3)
-    bodies.append(Body(mass=1, position=np.zeros(3), velocity=np.zeros(3)))
-    bodies.append(Body(mass=1, position=np.array([1, 1, 0]), velocity=np.array([0, 0, 0])))
-    bodies.append(Body(mass=1, position=np.array([-5, -5, 0]), velocity=np.array([0, 0, 0])))
-    dynamics = Dynamics(bodies)
-    energies = []
-    for _ in range(1000):
-        plt.clf()
-        kinematics = dynamics(0.1)
-        for body in bodies:
-            x, y, z = body('x')
-            plt.scatter(x, y)
-            plt.xlim(-10, 10)
-            plt.ylim(-10, 10)
-        plt.pause(0.01)
-        body = bodies[0]
-        other_bodies = [x for x in bodies if not x == body]
-        e = body.calculate_total_energies(other_bodies)
-        energies.append(e)
-    PE, KE, E, t = ([], [], [], [])
-    for i, energy in enumerate(energies):
-        t.append(i)
-        KE.append(energy['KE'])
-        PE.append(energy['PE'])
-        E.append(energy['E'])
-    KE = np.array(KE)
-    PE = np.array(PE)
-    plt.clf()
-    plt.plot(t, KE)
-    plt.plot(t, PE)
-    plt.plot(t, E)
-    plt.legend(['KE', 'PE', 'E'])
-    plt.show()
+    main()
