51312092744c151b43bc4a8aba6209b446db54c5
awang104-2 <awang104@berkeley.edu>
2025-07-15 02:08:29 -0700

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -4,27 +4,35 @@ MIT License
 Copyright (c) 2025 awang104
 """
 
-import src.nbp.dynamics as dynamics
-import src.nbp.simulation as simulation
+from nbp import dynamics
+from nbp import simulation
+from nbp import constants
 import random
 
 
 max_mass = 100
+constants.G = 1
 
 
-def main(n=10, graph=True):
+def main(n):
     bodies = []
     for i in range(n):
-        b = dynamics.Body(mass=random.random() * max_mass, position=[random.randint(5, 1275), random.randint(5, 715), random.randint(-1000, 1000)], velocity=[random.randint(0, 100), random.randint(0, 100), random.randint(0, 100)])
-        bodies.append(b)
-    bodies.append(dynamics.Body(mass=2e7, position=(640, 360, 0), velocity=(0, 0, 0)))
-    time, energy = simulation.simulate(bodies, bounded=True)
-    if graph:
-        simulation.energy_plot(time, energy)
+        body = dynamics.Body(
+            random.randint(1, 20),
+            (random.random() * 1280, random.random() * 720, 0),
+            (random.random() * 200 - 100, random.random() * 200 - 100, 0)
+        )
+        bodies.append(body)
+    body = dynamics.Body(
+        1e7,
+        (690, 360, 0),
+        (0, 0, 0)
+    )
+    bodies.append(body)
+    system = dynamics.system(bodies)
+    simulation.simulate(system, dynamics.rkf4)
 
 
 if __name__ == '__main__':
-    main(50, True)
-
-
+    main(3)
 
