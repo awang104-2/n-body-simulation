41202766f5f4b0c7747b0db2fd401a90af935f27
awang104-2 <awang104@berkeley.edu>
2025-07-15 02:10:55 -0700

    Edited simulate function to respond to adaptive time steps.

--- src/nbp/simulation.py
+++ src/nbp/simulation.py
@@ -4,8 +4,9 @@ MIT License
 Copyright (c) 2025 awang104
 """
 from typing import Iterable
+from nbp import constants
 from nbp import dynamics
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import pygame
 
 
@@ -16,32 +17,31 @@ def draw(surface: pygame.Surface, bodies: Iterable[dynamics.Body], scale: float 
     :param bodies: List of Body instances
     :param scale: Meters per pixel, by default 1 m/px
     """
-    max_mass = max(map(lambda b: b.m, bodies))
     for b in bodies:
         x, y, z = (b.x / scale).astype(int)
         pygame.draw.circle(surface, b.color, (x, y), 5)
 
 
-def simulate(bodies: Iterable[dynamics.Body], bounded: bool = False, scale: float = 1):
+def simulate(system: dynamics.SystemLike, integrator=dynamics.leapfrog, scale: float = 1):
     """
-    :param bodies:
-    :param bounded:
+    Simulates n-body interaction with PyGame.
+    :param system:
+    :param integrator:
     :param scale:
     """
     pygame.init()
     screen = pygame.display.set_mode((1280, 720), flags=pygame.SCALED)
     clock = pygame.time.Clock()
     running = True
+    t, dt = 0, 0.01
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill('black')
-        draw(screen, bodies)
+        draw(screen, system.bodies, scale)
         pygame.display.flip()
-        dynamics.leapfrog(bodies=bodies, dt=0.01, forces=[dynamics.gravity])
-        if bounded:
-            dynamics.boundary(bodies, [0, 1280], [0, 720])
+        t, dt = integrator(system, t, dt)
         clock.tick(60)
     pygame.quit()
 
@@ -54,4 +54,6 @@ def energy_plot(t: Iterable[float], e: Iterable[float]):
     plt.xlabel('Time (s)')
     plt.ylabel('Energy (J)')
     plt.show()
-    
+
+
+
